8b74143f87d8f56592dfe37e44a7b75056cc7ba4
adarshkm5555 <2019071004@mmmut.ac.in>
2022-04-19 22:10:37 +0530

    first commit

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,8 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract TodoList{
+contract TodoList {
+    uint256 public taskCount = 0;
 
-    uint public taskCount = 0;
+    struct Task {
+        uint256 id;
+        string content;
+        bool completed;
+    }
 
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("Check Out this!!");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
